8455773f6b36e63807e672f8ce740581be440456
Thitipan-James <thitipunsaelee@gmail.com>
2025-09-30 11:58:48 +0700

    config vite

--- svelte.config.js
+++ svelte.config.js
@@ -2,18 +2,15 @@
 import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter(), // หรือ adapter({ pages: 'build', assets: 'build' })
-		prerender: {
-			entries: ['*'], // <-- แทนที่ default
-			crawl: true // ค้นหา routes อัตโนมัติ (ถ้าต้องการ)
-		}
-	}
-};
-
 export default {
 	define: {
 		'import.meta.env.VITE_LIFF_ID': JSON.stringify(process.env.VITE_LIFF_ID)
+	},
+	kit: {
+		adapter: adapter() // หรือ adapter({ pages: 'build', assets: 'build' })
+		// prerender: {
+		// 	entries: ['*'], // <-- แทนที่ default
+		// 	crawl: true // ค้นหา routes อัตโนมัติ (ถ้าต้องการ)
+		// }
 	}
 };
